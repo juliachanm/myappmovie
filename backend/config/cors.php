e4df7d3f863b2013f3be7b4a8389eb07cf0bccc4
juliachanm <juliamch0887@gmail.com>
2025-12-01 09:17:12 -0600

    Actualizar config/cors.php y ajustes para frontend en Railway

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,7 @@
 return [
      'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:4200', 'http://127.0.0.1:4200'],
+    'allowed_origins' => ['http://localhost:4200', 'http://127.0.0.1:4200','https://myappmovie-production.up.railway.app' ],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
